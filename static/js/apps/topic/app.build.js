363bdf49e7954ff26bf7a96d44aa9bd94db29c16
Jeffrey Mullins <jmullins@30and30.com>
2012-02-14 17:45:36 -0500

    Fixed typo in app.build.js (chat => topic)

--- static/js/apps/topic/app.build.js
+++ static/js/apps/topic/app.build.js
@@ -1,7 +1,7 @@
 ({
         appDir: ".",
         baseUrl: "../../",
-        dir: "../../../../static_collected/js/apps/chat",
+        dir: "../../../../static_collected/js/apps/topic",
         modules: [
                 { name: "apps/topic/main" }
         ],
