1d7177d085d0841ac40c682ea57188ea6dab42ab
Jeffrey Mullins <jmullins@30and30.com>
2012-03-06 10:09:18 -0500

    Renamed bootstrap minified library to follow convention. Added bootstrap-min to loader-min

--- static/js/loader/loader-min.js
+++ static/js/loader/loader-min.js
@@ -1,5 +1,6 @@
 define([
     'order!3ps/jquery/jquery-min',
+    'order!3ps/bootstrap/bootstrap-min',
     'order!3ps/underscore/underscore-min',
     'order!3ps/backbone/backbone-min',
     'order!3ps/highcharts/js/highcharts',
