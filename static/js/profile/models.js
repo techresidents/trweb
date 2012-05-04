3884d64d859186fdaf18698bc68ad46618e96eee
bmullins <bmullins@30and30.com>
2012-05-04 10:21:27 -0400

    Add localstorage declaration to js models. Add comments to js models. Tweak UI text in the profile.

--- static/js/profile/models.js
+++ static/js/profile/models.js
@@ -4,6 +4,13 @@ define([
     'Backbone',
 ], function($, _, Backbone) {
 
+    /**
+     * Skill model is used to represent a user's skill in
+     * any technology such as a language or framework.
+     * @constructor
+     * @param {Object} attributes Optional model attributes
+     * @param {Object} options Optional model options.
+     */
     var Skill = Backbone.Model.extend({
 
         idAttribute: "name",
@@ -16,7 +23,7 @@ define([
             };
         },
 
-        initialize: function() {
+        initialize: function(attributes, options) {
 
         },
 
@@ -47,9 +54,20 @@ define([
     });
 
     var SkillCollection = Backbone.Collection.extend({
+        /**
+         * Skill collection is persisted locally.
+         */
+        localStorage: new Backbone.LocalStorage('SkillCollection'),
         model: Skill
     });
 
+    /**
+     * PositionType represents a specific type of job
+     * such as Developer, Manager, etc.
+     * @constructor
+     * @param {Object} attributes Optional model attributes
+     * @param {Object} options Optional model options.
+     */
     var PositionType = Backbone.Model.extend({
 
         // This object will use the PositionType.id from the SQL db.
@@ -62,7 +80,7 @@ define([
             };
         },
 
-        initialize: function() {
+        initialize: function(attributes, options) {
 
         },
 
@@ -76,9 +94,21 @@ define([
     });
 
     var PositionTypeCollection = Backbone.Collection.extend({
+        /**
+         * PositionType collection is persisted locally.
+         */
+        localStorage: new Backbone.LocalStorage('PositionTypeCollection'),
         model: PositionType
     });
 
+    /**
+     * PositionPreference is used to represent a user's
+     * job preference when being solicited for new job
+     * opportunities.
+     * @constructor
+     * @param {Object} attributes Optional model attributes
+     * @param {Object} options Optional model options.
+     */
     var PositionPreference = Backbone.Model.extend({
 
         defaults : function() {
@@ -88,7 +118,7 @@ define([
             };
         },
 
-        initialize: function() {
+        initialize: function(attributes, options) {
         },
 
         positionTypeId: function() {
@@ -105,9 +135,21 @@ define([
     });
 
     var PositionPrefCollection = Backbone.Collection.extend({
+        /**
+         * PositionPref collection is persisted locally.
+         */
+        localStorage: new Backbone.LocalStorage('PositionPrefCollection'),
         model: PositionPreference
     });
 
+    /**
+     * TechnologyPreference is used to represent a user's
+     * technology preference when being solicited for new job
+     * opportunities.
+     * @constructor
+     * @param {Object} attributes Optional model attributes
+     * @param {Object} options Optional model options.
+     */
     var TechnologyPreference = Backbone.Model.extend({
 
         defaults : function() {
@@ -118,7 +160,7 @@ define([
             };
         },
 
-        initialize: function() {
+        initialize: function(attributes, options) {
 
         },
 
@@ -136,9 +178,21 @@ define([
     });
 
     var TechnologyPrefCollection = Backbone.Collection.extend({
+        /**
+         * TechnologyPref collection is persisted locally.
+         */
+        localStorage: new Backbone.LocalStorage('TechnologyPrefCollection'),
         model: TechnologyPreference
     });
 
+    /**
+     * LocationPreference is used to represent a user's
+     * location preference when being solicited for new job
+     * opportunities.
+     * @constructor
+     * @param {Object} attributes Optional model attributes
+     * @param {Object} options Optional model options.
+     */
     var LocationPreference = Backbone.Model.extend({
 
         defaults : function() {
@@ -151,7 +205,7 @@ define([
             };
         },
 
-        initialize: function() {
+        initialize: function(attributes, options) {
 
         },
 
@@ -177,10 +231,27 @@ define([
     });
 
     var LocationPrefCollection = Backbone.Collection.extend({
+        /**
+         * LocationPref collection is persisted locally.
+         */
+        localStorage: new Backbone.LocalStorage('LocationPrefCollection'),
         model: LocationPreference
     });
 
+    /**
+     * NotificationPreference is used to represent a user's
+     * job notification preferences when being solicited for new job
+     * opportunities.
+     * @constructor
+     * @param {Object} attributes Optional model attributes
+     * @param {Object} options Optional model options.
+     */
     var NotificationPreference = Backbone.Model.extend({
+
+        /**
+         * NotificationPreference model is persisted locally.
+         */
+        localStorage: new Backbone.LocalStorage('NotificationPreference'),
 
         defaults : function() {
             return {
@@ -188,10 +259,15 @@ define([
             };
         },
 
-        initialize: function() {
+        initialize: function(attributes, options) {
 
         },
 
+        /**
+         * Boolean used to indicate if a user wants to be
+         * notified about new job opportunities.
+         * @return {Boolean} returns true if user want notifications
+         */
         emailNewJobOpps: function() {
             return this.get("emailNewJobOpps");
         },
