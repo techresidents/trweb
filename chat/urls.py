4fb8f05d3ae8e2176b10e640bc3ecdbd773c2344
Jeffrey Mullins <jmullins@30and30.com>
2012-06-13 09:52:33 -0400

    Fixing ordering of urls in chat/urls

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('chat.views',
     url(r'^$', 'list'),
+    url(r'^create$', 'create'),
     url(r'^(?P<encoded_chat_session_id>\w+)$', 'chat'),
     url(r'^(?P<encoded_chat_session_id>\w+)/feedback$', 'feedback'),
-    url(r'^create/$', 'create'),
 )
