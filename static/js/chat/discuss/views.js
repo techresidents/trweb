4fe68b7c81d517563f9690f288221fea4964d508
bmullins <bmullins@30and30.com>
2012-06-21 13:17:40 -0400

    Started updating the chat app styling.

--- static/js/chat/discuss/views.js
+++ static/js/chat/discuss/views.js
@@ -5,7 +5,18 @@ define([
     'chat/agenda/models',
     'timer/views',
     'text!chat/discuss/templates/discuss.html',
-], function($, _, view, models, timer, discuss_template) {
+    'text!chat/discuss/templates/discuss_title.html',
+    'text!chat/discuss/templates/discuss_controls.html'
+], function(
+    $,
+    _,
+    view,
+    models,
+    timer,
+    discuss_parent_template,
+    discuss_title_template,
+    discuss_controls_template)
+{
 
     /**
      * Discuss View Events
@@ -16,13 +27,82 @@ define([
     };
 
     /**
-     * Disucss view.
+     * Parent Discuss view.
+     * @constructor
+     * @param {Object} options View options
+     */
+    var DiscussView = view.View.extend({
+
+        titleSelector: '#discussion-title',
+
+        controlsSelector: '#discussion-controls',
+
+        tagSelector: '',
+
+        initialize: function() {
+            this.template = _.template(discuss_parent_template);
+            this.users = this.options.users;
+            this.tags = this.options.tags;
+        },
+
+        render: function() {
+
+            this.$el.html(this.template());
+
+            new DiscussTitleView({
+                el: this.$(this.titleSelector),
+                model: this.model,
+            }).render();
+
+
+            new DiscussControlsView({
+                el: this.$(this.controlsSelector),
+                model: this.model,
+            }).render();
+
+            /*
+            new TagView({
+                el: this.$(this.tagSelector),
+                model: this.model,
+                minutes: this.model.minutes(),
+                tags: this.tags,
+                users: this.users,
+            }).render();
+            */
+
+            return this;
+        },
+    });
+
+    /**
+     * Discussion title view.
+     * Responsible for rendering the discussion topic.
+     * @constructor
+     * @param {Object} options
+     *   templateSelector: html template selector (optional)
+     */
+    var DiscussTitleView = view.View.extend({
+
+        initialize: function() {
+            this.template = _.template(discuss_title_template);
+            this.model.bind('change:activeTopic', this.render, this);
+        },
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
+
+    /**
+     * Discussion controls view.
+     * Responsible for rendering a view to control the discussion (e.g. start, next topic, etc)
      * @constructor
      * @param {Object} options
      *   templateSelector: html template selector (optional)
      *   timerSelector: selector for timer view (optional)
      */
-    var DiscussView = view.View.extend({
+    var DiscussControlsView = view.View.extend({
 
         timerSelector: '#discuss-timer',
 
@@ -32,7 +112,55 @@ define([
         },
 
         initialize: function() {
-            this.template = _.template(discuss_template);
+            this.template = _.template(discuss_controls_template);
+            this.model.bind('change:activeTopic', this.render, this);
+            this.timer = null;
+        },
+
+        render: function() {
+
+            this.$el.html(this.template(this.model.toJSON()));
+
+            var activeTopic = this.model.activeTopic();
+            if(activeTopic) {
+                this.timer = new timer.DurationTimerView({
+                    el: this.$(this.timerSelector),
+                    duration: activeTopic.durationMs(),
+                    interval: 500,
+                });
+                this.timer.render();
+                this.timer.start();
+            }
+            return this;
+        },
+
+        next: function() {
+            this.triggerEvent(EVENTS.NEXT);
+        },
+
+        start: function() {
+            this.triggerEvent(EVENTS.START);
+        },
+    });
+
+    /**
+     * Disucss view.
+     * @constructor
+     * @param {Object} options
+     *   templateSelector: html template selector (optional)
+     *   timerSelector: selector for timer view (optional)
+     */
+    var DiscussOldView = view.View.extend({
+
+        timerSelector: '#discuss-timer',
+
+        events: {
+            "click .next": "next",
+            "click .start": "start",
+        },
+
+        initialize: function() {
+            this.template = _.template(discuss_parent_template);
             this.model.bind('change:activeTopic', this.render, this);
             this.timer = null;
         },
