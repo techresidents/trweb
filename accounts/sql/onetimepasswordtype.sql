5c142802f18c23f93ad5e83944e657a50fbbb12c
Jeffrey Mullins <jmullins@techresidents.com>
2012-08-20 10:12:19 -0400

    Adding support for time based one time passwords.

---
+++ accounts/sql/onetimepasswordtype.sql
@@ -0,0 +1,2 @@
+INSERT INTO accounts_one_time_password_type (name, description) VALUES ('HOTP', 'HMAC based one time password (RFC 4226)');
+INSERT INTO accounts_one_time_password_type (name, description) VALUES ('TOTP', 'Time based one time password (RFC 6238)');
