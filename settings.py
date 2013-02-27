c9fa378ccb83969ac01269bdd2bf337103d7f54e
Jeffrey Mullins <jmullins@techresidents.com>
2013-02-27 13:26:28 -0500

    - Adding ALLOWED_HOSTS entries (required when debug is False) - Making logging filters and context processors more robust

--- settings.py
+++ settings.py
@@ -23,6 +23,9 @@ DATABASES = {
     }
 }
 
+#Allowed hosts
+ALLOWED_HOSTS = ['.techresidents.com', 'localdev']
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
