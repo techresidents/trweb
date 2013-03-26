4aae6e4d4a43232e9532a7a92773279324bd393e
bmullins <bmullins@techresidents.com>
2013-03-26 13:47:04 -0400

    Code clean up.

--- common/decorators.py
+++ common/decorators.py
@@ -2,34 +2,19 @@ from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import REDIRECT_FIELD_NAME
 
 def staff_required(function, redirect_field_name=REDIRECT_FIELD_NAME):
-    def check_is_staff(user):
-        ret = False
-        if user.is_authenticated() and user.is_staff:
-            ret = True
-        return ret
     wrapper = user_passes_test(
-            check_is_staff,
-            redirect_field_name=redirect_field_name)
+        lambda u: u.is_authenticated() and u.is_staff,
+        redirect_field_name=redirect_field_name)
     return wrapper(function)
 
 def employer_required(function, redirect_field_name=REDIRECT_FIELD_NAME):
-    def check_is_employer(user):
-        ret = False
-        if user.is_authenticated() and user.is_employer:
-            ret = True
-        return ret
     wrapper = user_passes_test(
-        check_is_employer,
+        lambda u: u.is_authenticated() and u.is_employer,
         redirect_field_name=redirect_field_name)
     return wrapper(function)
 
 def developer_required(function, redirect_field_name=REDIRECT_FIELD_NAME):
-    def check_is_developer(user):
-        ret = False
-        if user.is_authenticated() and user.is_developer:
-            ret = True
-        return ret
     wrapper = user_passes_test(
-            check_is_developer,
-            redirect_field_name=redirect_field_name)
+        lambda u: u.is_authenticated() and u.is_developer,
+        redirect_field_name=redirect_field_name)
     return wrapper(function)
