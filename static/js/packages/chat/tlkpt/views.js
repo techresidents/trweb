f7ecdf2daf7e717137c3c6307f56a48255dd8b88
bmullins <bmullins@techresidents.com>
2013-05-16 17:57:51 -0400

    Added beginnings of the talking points views.

--- static/js/packages/chat/tlkpt/views.js
+++ static/js/packages/chat/tlkpt/views.js
@@ -7,6 +7,7 @@ define([
     'ui',
     '../topic/views',
     'text!./templates/talking_points_summary.html',
+    'text!./templates/add_tlkpt.html',
     'text!./templates/tlkpt.html'
 ], function(
     $,
@@ -17,10 +18,39 @@ define([
     ui,
     topic_views,
     talking_points_summary_template,
+    add_talking_point_template,
     talking_point_template) {
+
+
+    /**
+     * AddTalkingPointView View
+     * @constructor
+     * @param {Object} options
+     *   topicLevel: {Number} topic level
+     */
+    var AddTalkingPointView = core.view.View.extend({
+
+        events: {
+        },
+
+        initialize: function(options) {
+            this.topicLevel = options.topicLevel;
+            this.template =  _.template(add_talking_point_template);
+        },
+
+        render: function() {
+            var context = {
+                level: this.topicLevel
+            };
+            this.$el.html(this.template(context));
+            return this;
+        }
+    });
 
     /**
      * TalkingPointView View
+     * A TalkingPointView consists of one topic (no sub-topics) and
+     * any associated talking points.
      * @constructor
      * @param {Object} options
      *   model: {Topic} model (required)
@@ -33,6 +63,7 @@ define([
         childViews: function() {
             return [
                 this.topicView,
+                this.addTalkingPointView,
                 this.talkingPointsListView
             ];
         },
@@ -41,8 +72,13 @@ define([
             this.model = options.model;
             this.template =  _.template(talking_point_template);
 
+            //load data
+            // for the topic, load this user's talking points
+            this.tpCollection = null;
+
             //child views
             this.topicView = null;
+            this.addTalkingPointView = null;
             this.talkingPointsListView = null;
             this.initChildViews();
         },
@@ -51,11 +87,19 @@ define([
             this.topicView = new topic_views.TopicView({
                 model: this.model
             });
+            this.addTalkingPointView = new AddTalkingPointView({
+                topicLevel: this.model.get_level()
+            });
+//            this.talkingPointsListView = new ui.collection.views.CollectionView({
+//                collection: this.tpCollection,
+//                viewFactory: new core.factory.Factory(TalkingPointInputView, {})
+//            });
         },
 
         render: function() {
             this.$el.html(this.template());
             this.append(this.topicView);
+            this.append(this.addTalkingPointView);
             return this;
         }
     });
@@ -120,6 +164,7 @@ define([
     });
 
     return {
+        AddTalkingPointView: AddTalkingPointView,
         TalkingPointView: TalkingPointView,
         TalkingPointsSummaryView: TalkingPointsSummaryView
     };
