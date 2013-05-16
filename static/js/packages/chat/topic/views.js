e94e6b6ea4a5aa74b23841f487be979b2037c7b9
bmullins <bmullins@techresidents.com>
2013-05-16 12:29:16 -0400

    Created topic details page in js

--- static/js/packages/chat/topic/views.js
+++ static/js/packages/chat/topic/views.js
@@ -2,17 +2,22 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'core'
+    'core',
+    'api',
+    'text!./templates/details.html'
 ], function(
     $,
     _,
     Backbone,
-    core) {
+    core,
+    api,
+    topic_details_template) {
 
     /**
      * Topic View
      * @constructor
      * @param {Object} options
+     *   model: {Topic} model (required)
      */
     var TopicView = core.view.View.extend({
 
@@ -20,9 +25,26 @@ define([
         },
 
         initialize: function(options) {
+            this.model = options.model;
+            this.template =  _.template(topic_details_template);
+            this.modelWithRelated = ['tree'];
+
+            //load data
+            this.loader = new api.loader.ApiLoader([
+                { instance: this.model, withRelated: this.modelWithRelated }
+            ]);
+            this.listenTo(this.loader, 'loaded', this.render);
+            this.loader.load();
         },
         
         render: function() {
+            var context = {
+                topic_tree: this.model.get_tree().toJSON(),
+                fmt: this.fmt // date formatting
+            };
+            if (this.loader.isLoaded()) {
+                this.$el.html(this.template(context));
+            }
             return this;
         }
     });
