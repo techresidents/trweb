fa5d8fdf83ad26720f4feacae16366fdaa6cd89e
bmullins <bmullins@techresidents.com>
2013-08-14 17:34:28 -0400

    Added help view to developer profile job hunt button.

--- static/js/packages/ui/help/views.js
+++ static/js/packages/ui/help/views.js
@@ -25,6 +25,8 @@ define(/** @exports ui/help/views */[
          * @param {string} [options.placement='bottom'] Placement string -
          *   top, right, bottom, left  - indicating where to position 
          *   view.
+         * @param {string} [options.iconClasses='icon-question-sign icon-large']
+         *   String to indicate which icon to display
          * @param {View|Factory} [options.viewOrFactory] View object or Factory
          *   returning a View object to use for the tooltip content.
          * @param {boolean} [options.delayViewCreate=false] Flag indicating 
@@ -45,7 +47,7 @@ define(/** @exports ui/help/views */[
             options = _.extend({
                 template: help_template,
                 placement: 'right',
-                iconClasses: 'icon-question-sign icon-large', 
+                iconClasses: 'icon-question-sign icon-large',
                 delayViewCreate: false,
                 showDelay: 200,
                 hideDelay: 100,
